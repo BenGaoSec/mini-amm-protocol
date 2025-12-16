87735fdf2d915c44ead4801e3b2fe9ebe7ab6dbe
BenGaoSec <bengaoloveai@gmail.com>
2025-12-16 22:20:23 +0800

    feat: update amm factory/pair

--- src/core/MiniAmmFactory.sol
+++ src/core/MiniAmmFactory.sol
@@ -55,11 +55,37 @@ contract MiniAmmFactory is IMiniAmmFactory {
     // =============================================================
     //                         CREATE PAIR
     // =============================================================
-
+    function createPair(address tokenA, address tokenB) external override returns (address pair){
+        if(tokenA == tokenB) revert IdenticalAddresses();
+        if(tokenA == address(0) || tokenB == address(0)) revert ZeroAddress();
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB ) :(tokenB, tokenA);
+        if(getPair[token0][token1] != address(0)) revert PairExists();
+        bytes32 salt = keccak256(abi.encodePacked(token0,token1));
+        //Deploy pair deterministically with CREATE2
+        pair = address(new MiniAmmPair{salt : salt}());
+        // Initialize
+        IMiniAmmPair(pair).initialize(token0,token1);
+        //BookKeeping in both direction
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair;
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
 
     // =============================================================
     //                     FEE PARAM ADMIN
     // =============================================================
 
-  
+    function setFeeTo(address _feeTo) external override {
+        if(msg.sender != feeToSetter) revert Forbidden();
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external override{
+        if(msg.sender != feeToSetter) revert Forbidden();
+        if(_feeToSetter == address(0)) revert ZeroAddress();
+        feeToSetter = _feeToSetter;
+    }
+
+
 }
